27fae8f0c8e0ef926fd4d6e737e1de7b95bbcefe
takumihashimoto <hashimotot235@gmail.com>
2017-04-18 08:07:49 +0000

    削除ページ追加、リンク追加、フラッシュメッセージ関連のファイル修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -29,9 +29,24 @@ class TasksController < ApplicationController
   end
   
   def update
+    @task = Task.find(params[:id])
+    
+    if @task.update(task_params)
+      flash[:success] = 'Task は正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task は更新されませんでした'
+      render :edit
+    end
+    
   end
   
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
   end
   
   private
