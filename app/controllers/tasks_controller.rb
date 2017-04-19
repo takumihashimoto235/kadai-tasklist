76d86533af078e5cc26b715b5a05c4609256490f
takumihashimoto <hashimotot235@gmail.com>
2017-04-19 01:34:59 +0000

    共通化

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :set_message, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
@@ -50,6 +51,10 @@ class TasksController < ApplicationController
   end
   
   private
+  
+  def set_task
+    @task = Task.find(params[:id])
+  end
   
   def task_params
     params.require(:task).permit(:content)
