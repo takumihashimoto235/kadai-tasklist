ec861470b0cf2daa8642cc9a2a600f044d8a5569
takumihashimoto <hashimotot235@gmail.com>
2017-04-19 02:24:05 +0000

    バリデーション追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,12 @@
 class TasksController < ApplicationController
-  before_action :set_message, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
+    #@task = Task.find(params[:id])
   end
 
   def new
@@ -26,11 +26,11 @@ class TasksController < ApplicationController
   end
   
   def edit
-    @task = Task.find(params[:id])
+    #@task = Task.find(params[:id])
   end
   
   def update
-    @task = Task.find(params[:id])
+    #@task = Task.find(params[:id])
     
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
